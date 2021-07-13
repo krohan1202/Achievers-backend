db353e22d687382945331dc1d1f8b3e5ad8e7bb1
Rohan Karmakar <krohan1202@gmail.com>
2021-07-13 13:59:15 +0530

    Update server

--- server.js
+++ server.js
@@ -104,7 +104,6 @@ app.get('/',(req,res) => {
 	return res.send('Route Working.');
 });
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
+app.listen(process.env.PORT || 5000, () =>{
     console.log('Server is running on port', PORT);
 })
