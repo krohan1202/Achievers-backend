d2edc6799c17d4a308dd912f4e727e720ffbca5b
Rohan Karmakar <krohan1202@gmail.com>
2021-07-13 14:26:13 +0530

    Issue resolve try 1

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require("express-validator");
-const { getNewsletterEmail } = require("../controllers/newsletter");
+const { getNewsletterEmail } = require("../controllers/Newsletter");
 
 router.post(
   "/",
